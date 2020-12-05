d08f3f6c51f8531938feed3f341d95fa7ca9806f
Codeblogger <m.furkangulsen@hotmail.com>
2020-12-05 13:53:31 +0300

    train

--- chat.py
+++ chat.py
@@ -23,3 +23,28 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
+bot_name = "Furkan"
+print("Let's chat! type 'quit' to exit")
+while True:
+	sentence = input("You: ")
+	if sentence == "quit":
+		break
+
+	sentence = tokenize(sentence)
+	X = bag_of_words(sentence, all_words)
+	X = X.reshape(1, X.shape[0]) 
+	X = torch.from_numpy()
+
+	output = model(X)
+	_, predicted = torch.max(output, dim=1)
+	tag = tags[predicted.item()]
+
+	probs = torch.softmax(output, dim=1)
+	prob = probs[0][predicted.item]
+
+	if prob.item() > 0.75:
+		for intent in intents['intents']:
+			if tag == intent['tag']:
+				print(f"{bot_name}: {random.choice(intent['responses'])}")
+	else:
+		print(f"{bot_name}: I do not understand...")
