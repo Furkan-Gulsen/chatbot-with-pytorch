d6eca32be75523240d779170b3c473327dd3c11c
Codeblogger <m.furkangulsen@hotmail.com>
2020-12-05 12:22:02 +0300

    train

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 class NeuralNet(nn.Module):
 	def __init__(self, input_size, hidden_size, num_classes):
+		super(NeuralNet, self).__init__()
 		self.l1 = nn.Linear(input_size, hidden_size)
 		self.l2 = nn.Linear(hidden_size, hidden_size)
 		self.l3 = nn.Linear(input_size, num_classes)
