71fb2b6c156ab8920de25a65f689d15c72c87933
Codeblogger <m.furkangulsen@hotmail.com>
2020-12-05 12:42:01 +0300

    train

--- train.py
+++ train.py
@@ -43,16 +43,18 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 class ChatDataset(Dataset):
-	def __init__(self):
-		self.n_samples = len(X_train)
-		self.x_data = X_train
-		self.y_data = y_train
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
 
-	def __getitem__(self, index):
-		return self.x_data[index], self.y_data[index]
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
 
-	def __len__(self):
-		return self.n_samples
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
 
 # Hyperparameters
 batch_size = 8
